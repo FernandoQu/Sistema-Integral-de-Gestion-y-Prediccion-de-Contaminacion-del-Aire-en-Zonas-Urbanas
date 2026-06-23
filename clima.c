7817055b0397021f64958a49c9a983a98ade7825
FernandoQu <fernando.quezada@udla.edu.ec>
2026-06-22 23:07:41 -0500

    Se elimino la variable de ingreso de nombres de las zonas por nombres preestablecidos

--- clima.c
+++ clima.c
@@ -4,43 +4,6 @@
 #include "validaciones.h"
 #include "clima.h"
 
-void configurarNombresZonas(ZonaUrbana ciudades[], int totalZonas) {
-    for (int i = 0; i < totalZonas; i++) {
-        int esValido = 0;
-        
-        do {
-            printf("Ingrese el nombre para la zona %d (Solo letras, max 49 caracteres): ", i + 1);
-            
-            //se lee los nombres de las zonas que el usuario ingresa 
-            fgets(ciudades[i].zonas, 50, stdin);
-
-            //al usar el fgets se debe eliminar el enter (\n) porque el fgets lo lee tambien
-            //tambien se crea una variable para comprobar que no se dejo el campo vacio
-            int longitud = 0;
-            while (ciudades[i].zonas[longitud] != '\0') {
-                if (ciudades[i].zonas[longitud] == '\n') {
-                    ciudades[i].zonas[longitud] = '\0';
-                    break;
-                }
-                longitud++;
-            }
-
-            //se comprueba que solo se ingrese letras y se comprueba la longitud 
-            //tambien se comprueba si la longitud es > 0 para comprobar que se ingreso algo
-            //validarSoloLetras esta en la linea 30 de validaciones.h
-            //validarLongitudMaxima esta en la linea 17 de validaciones.h
-            if (validarSoloLetras(ciudades[i].zonas) && 
-                validarLongitudMaxima(ciudades[i].zonas, 49) && 
-                longitud > 0) {
-                esValido = 1;
-            } else {
-                printf("Nombre invalido. Solo se puede usar solo letras y no dejar el campo vacio.\n");
-            }
-        } while (!esValido);
-    }
-    printf("\nLas %d zonas han sido nombradas y configuradas correctamente.\n", totalZonas);
-}
-
 void ingresarDatosActuales(ZonaUrbana *zona) {
     printf("\n--- Ingreso de datos para la zona: %s ---\n", zona->zonas);
 
