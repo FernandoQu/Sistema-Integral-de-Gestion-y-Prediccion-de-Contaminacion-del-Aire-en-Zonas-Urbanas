24b9836fe5071d08daea717f70bb9669790ee1ba
FernandoQu <fernando.quezada@udla.edu.ec>
2026-06-22 23:16:03 -0500

    Se modifico el validaciones para un mejor rendimiento

--- validaciones.c
+++ validaciones.c
@@ -17,41 +17,65 @@ int validarDecimal(float valor, float min, float max) {
 void leerEnteroValidado(char mensaje[], int min, int max, int *resultado) {
     int temporal = 0;
     int esValido = 0;
-    int c;
+    char c;
 
     do {
         printf("%s", mensaje);
-        
-        int valoresLeidos = scanf("%d", &temporal);
 
-        while ((c = getchar()) != '\n' && c != EOF);
+        if (scanf("%d", &temporal) == 1) {
 
-        if (valoresLeidos == 1 && validarEntero(temporal, min, max)) {
-            esValido = 1;
-            *resultado = temporal;
+            c = getchar();
+
+            if (c == '\n') {
+                if (validarEntero(temporal, min, max)) {
+                    esValido = 1;
+                    *resultado = temporal;
+                } else {
+                    printf("[!] Dato fuera de rango (%d a %d). Intente de nuevo.\n", min, max);
+                }
+            } else {
+
+                printf("[!] Error: Ingreso caracteres no validos junto al numero.\n");
+                while (c != '\n' && c != EOF) {
+                    c = getchar();
+                }
+            }
         } else {
-            printf("Dato fuera de rango o invalido, intente de nuevo\n");
+
+            printf("[!] Error: Debe ingresar un valor numerico.\n");
+            while ((c = getchar()) != '\n' && c != EOF);
         }
     } while (esValido == 0);
 }
 
-
 void leerDecimalValidado(char mensaje[], float min, float max, float *resultado) {
     float temporal = 0.0;
     int esValido = 0;
-    int c;
+    char c;
 
     do {
-        printf("%s\n", mensaje);
-        int valoresLeidos = scanf("%f", &temporal);
+        printf("%s", mensaje);
         
-        while ((c = getchar()) != '\n' && c != EOF);
-
-        if (valoresLeidos == 1 && validarDecimal(temporal, min, max)) {
-            esValido = 1;
-            *resultado = temporal; 
+        if (scanf("%f", &temporal) == 1) {
+            
+            c = getchar();
+            
+            if (c == '\n') {
+                if (validarDecimal(temporal, min, max)) {
+                    esValido = 1;
+                    *resultado = temporal; 
+                } else {
+                    printf("[!] Dato fuera de rango (%.1f a %.1f). Intente de nuevo.\n", min, max);
+                }
+            } else {
+                printf("[!] Error: Ingreso caracteres no validos junto al decimal.\n");
+                while (c != '\n' && c != EOF) {
+                    c = getchar();
+                }
+            }
         } else {
-            printf("Dato fuera de rango o invalido, intente de nuevo\n");
+            printf("[!] Error: Debe ingresar un valor numerico.\n");
+            while ((c = getchar()) != '\n' && c != EOF);
         }
     } while (esValido == 0);
 }
