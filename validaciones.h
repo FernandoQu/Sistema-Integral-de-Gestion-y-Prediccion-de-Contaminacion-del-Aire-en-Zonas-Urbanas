d41ca71e7935eaf805235dc1de712749a9182256
FernandoQu <fernando.quezada@udla.edu.ec>
2026-06-22 23:14:37 -0500

    Se elimino cadigo basura de validaciones

--- validaciones.h
+++ validaciones.h
@@ -3,15 +3,10 @@
 
 int validarEntero(int valor, int min, int max);
 int validarDecimal(float valor, float min, float max);
-int validarLongitudMaxima(char cadena[], int limiteMaximo);
-int validarSoloLetras(char cadena[]);
-
-int validarOpcionSN(char opcion);
-int validarLongitudExacta(char cadena[], int longitudEsperada);
 
 void leerEnteroValidado(char mensaje[], int min, int max, int *resultado);
 void leerDecimalValidado(char mensaje[], float min, float max, float *resultado);
 
-void leerOpcionSN(char mensaje[], char *resultado);
+
 
 #endif
